cc792bc7bb663b70255dcef0d54a54fc34810f32
Jahziel Villasana-Espinoza <jahziel@hirescholars.com>
2022-10-26 22:16:56 -0400

    feat: first release

---
+++ Formula/hemingway.rb
@@ -0,0 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Hemingway < Formula
+  desc "An economical RSS reader for your terminal."
+  homepage "https://github.com/jahzielv/hemingway"
+  url "https://github.com/jahzielv/hemingway/releases/download/v0.7.1/hemingway-mac-arm64.tar.gz"
+  sha256 "7a3304a0e7f2d042d0032a856314b86c13948787f0e190cbe1e390cf404140b9"
+  version "0.7.1"
+
+  def install
+    bin.install "hem"
+  end
+end
