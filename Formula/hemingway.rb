ff9cea97dd96a6ce0c28f8e1af5a9240489da757
Jahziel Villasana-Espinoza <jahziel@hirescholars.com>
2023-01-13 09:33:19 -0500

    chore: bump version

--- Formula/hemingway.rb
+++ Formula/hemingway.rb
@@ -3,7 +3,7 @@ class Hemingway < Formula
   homepage "https://github.com/jahzielv/hemingway"
   url "https://github.com/jahzielv/hemingway/releases/download/v0.7.1/hemingway-mac-arm64.tar.gz"
   sha256 "7a3304a0e7f2d042d0032a856314b86c13948787f0e190cbe1e390cf404140b9"
-  version "0.7.1"
+  version "0.7.2"
 
   def install
     bin.install "hem"
