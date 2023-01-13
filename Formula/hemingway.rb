1f7ac2f5fe1d13c3c5615c2b39d31c75d392a711
Jahziel Villasana-Espinoza <jahziel@hirescholars.com>
2023-01-13 09:40:16 -0500

    chore: sha update

--- Formula/hemingway.rb
+++ Formula/hemingway.rb
@@ -1,8 +1,8 @@
 class Hemingway < Formula
   desc "An economical RSS reader for your terminal."
   homepage "https://github.com/jahzielv/hemingway"
-  url "https://github.com/jahzielv/hemingway/releases/download/v0.7.1/hemingway-mac-arm64.tar.gz"
-  sha256 "7a3304a0e7f2d042d0032a856314b86c13948787f0e190cbe1e390cf404140b9"
+  url "https://github.com/jahzielv/hemingway/releases/download/v0.7.2/hemingway-mac-arm64.tar.gz"
+  sha256 "0e4ec337c6ab6476032c53fd53b6e13873570fe96a3515d127776bdbd9883a16"
   version "0.7.2"
 
   def install
